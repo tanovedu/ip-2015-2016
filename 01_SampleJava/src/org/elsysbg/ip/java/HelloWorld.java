aec980bb9cc81b1a97ce15446a9dea34e312460f
Plamen Tanov <tanovedu@gmail.com>
2015-10-09 17:06:49 +0300

    Added hint about keyboard shortcuts

--- 01_SampleJava/src/org/elsysbg/ip/java/HelloWorld.java
+++ 01_SampleJava/src/org/elsysbg/ip/java/HelloWorld.java
@@ -3,7 +3,7 @@ package org.elsysbg.ip.java;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// To delete whole line - use Ctrl+D
 		
 		// sysout expands to System.out.println() using Ctrl+Space
 		System.out.println("Hello world");
