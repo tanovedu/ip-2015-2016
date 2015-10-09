6ee6e23d6c24f860d10429c065d274d66defebb1
Plamen Tanov <tanovedu@gmail.com>
2015-10-09 17:06:17 +0300

    Hello world example

---
+++ 01_SampleJava/src/org/elsysbg/ip/java/HelloWorld.java
@@ -0,0 +1,12 @@
+package org.elsysbg.ip.java;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// sysout expands to System.out.println() using Ctrl+Space
+		System.out.println("Hello world");
+	}
+
+}
