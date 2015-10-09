63d16faae91a2e1f3b35765f09bea6eecdf1cb12
Plamen Tanov <tanovedu@gmail.com>
2015-10-09 17:13:21 +0300

    Hint about scope of the local variable

--- 01_SampleJava/src/org/elsysbg/ip/java/Variables.java
+++ 01_SampleJava/src/org/elsysbg/ip/java/Variables.java
@@ -3,11 +3,17 @@ package org.elsysbg.ip.java;
 public class Variables {
 
 	public static void main(String[] args) {
+		// scope of the variable is from definition
+		// to the end of the block, e.g. }
+		// error:
+		// a = 0;
 		// define local variable
 		int a;
-		
+
 		// assign value
 		a = 0;
 	}
+	// error:
+	// a = 0;
 
 }
