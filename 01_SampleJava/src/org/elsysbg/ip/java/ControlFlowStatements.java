5b833fcd943fa80d6f83c1cbcac02ee6ce7a2f6f
Plamen Tanov <tanovedu@gmail.com>
2015-10-09 18:09:41 +0300

    Hint for extracting methods

--- 01_SampleJava/src/org/elsysbg/ip/java/ControlFlowStatements.java
+++ 01_SampleJava/src/org/elsysbg/ip/java/ControlFlowStatements.java
@@ -3,6 +3,12 @@ package org.elsysbg.ip.java;
 public class ControlFlowStatements {
 
 	public static void main(String[] args) {
+		// in Eclipse: select statements that should be
+		// extracted to method, press Ctrl+1
+		ifExample();
+	}
+
+	private static void ifExample() {
 		// if without else
 		if (true) {
 			System.out.println("called");
