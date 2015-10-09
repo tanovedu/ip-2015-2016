2afb7429e67c09e1fc96888a675d2263c61d2745
Plamen Tanov <tanovedu@gmail.com>
2015-10-09 18:19:44 +0300

    Comparing objects - objects are not equal

---
+++ 01_SampleJava/src/org/elsysbg/ip/java/EqualsExample.java
@@ -0,0 +1,35 @@
+package org.elsysbg.ip.java;
+
+public class EqualsExample {
+	
+	private static final int WIDTH = 12;
+	private static final int HEIGHT = 10;
+
+	public static void main(String[] args) {
+		final Room room1 = new Room();
+		final Room room2 = new Room();
+		
+		room1.setHeight(HEIGHT);
+		room1.setWidth(WIDTH);
+		
+		room2.setHeight(HEIGHT);
+		room2.setWidth(WIDTH);
+		
+		System.out.println("room1 == room2");
+		// false
+		System.out.println(room1 == room2);
+
+		System.out.println("room1.equals(room2)");
+		// false
+		System.out.println(room1.equals(room2));
+		
+
+		System.out.println("room1 == room1");
+		// true
+		System.out.println(room1 == room1);
+
+		System.out.println("room1.equals(room1)");
+		// true
+		System.out.println(room1.equals(room1));
+	}
+}
