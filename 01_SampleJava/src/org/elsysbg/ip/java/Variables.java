e3ed958fd331d313c632175302ff51a144faac62
Plamen Tanov <tanovedu@gmail.com>
2015-10-09 17:12:48 +0300

    Variable definition

---
+++ 01_SampleJava/src/org/elsysbg/ip/java/Variables.java
@@ -0,0 +1,13 @@
+package org.elsysbg.ip.java;
+
+public class Variables {
+
+	public static void main(String[] args) {
+		// define local variable
+		int a;
+		
+		// assign value
+		a = 0;
+	}
+
+}
