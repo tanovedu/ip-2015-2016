3eaf00c3fc0fda9956f9028e3bc30d19ae6b6f5b
Plamen Tanov <tanovedu@gmail.com>
2015-10-09 17:38:04 +0300

    Keyboard shortcut to create local variable or field

--- 01_SampleJava/src/org/elsysbg/ip/java/ClassExample.java
+++ 01_SampleJava/src/org/elsysbg/ip/java/ClassExample.java
@@ -3,8 +3,11 @@ package org.elsysbg.ip.java;
 public class ClassExample {
 
 	public static void main(String[] args) {
+		// navigate cursor to Room(), press Ctrl+2 L
+		// to create local variable automatically
+		// press Ctrl+2 F to create class field
 		// create object of type Room
-		new Room();
+		Room room = new Room();
 
 	}
 
