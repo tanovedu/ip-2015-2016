cb6d479d0536078a7d9e9275fca7b5c67adea25a
Plamen Tanov <tanovedu@gmail.com>
2015-10-09 17:07:55 +0300

    Hint about shortcut for creating main()

--- 01_SampleJava/src/org/elsysbg/ip/java/HelloWorld.java
+++ 01_SampleJava/src/org/elsysbg/ip/java/HelloWorld.java
@@ -2,6 +2,7 @@ package org.elsysbg.ip.java;
 
 public class HelloWorld {
 
+	// main expands to public static void main... using Ctrl+Space
 	public static void main(String[] args) {
 		// To delete whole line - use Ctrl+D
 		
