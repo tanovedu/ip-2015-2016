84816216b5a9626c55230164766025c938f13e76
Plamen Tanov <tanovedu@gmail.com>
2015-11-06 18:45:00 +0200

    Server socket is immediately closed when stopServer is received

--- 02_SampleSockets/src/org/elsysbg/ip/sockets/EchoServer.java
+++ 02_SampleSockets/src/org/elsysbg/ip/sockets/EchoServer.java
@@ -3,6 +3,7 @@ package org.elsysbg.ip.sockets;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketException;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
@@ -13,33 +14,42 @@ public class EchoServer {
 	private final List<ClientHandler> clients =
 		Collections.synchronizedList(
 			new LinkedList<ClientHandler>());
+	private ServerSocket serverSocket;
 
 	public EchoServer(int port) {
 		this.port = port;
 	}
 
 	public void startServer() throws IOException {
-		setRunning();
-		final ServerSocket serverSocket =
-			new ServerSocket(port);
+		final ServerSocket localServerSocket = createServerSocket();
 		
 		// to synchronize access to the variable 'running'
 		while(isRunning()) {
-			final Socket socket = serverSocket.accept();
-			
+			final Socket socket;
+			try {
+				socket = localServerSocket.accept();
+			} catch (SocketException e) {
+				if (!localServerSocket.isClosed()) {
+					throw e;
+				}
+				break;
+			}
+
 			final ClientHandler client =
 				new ClientHandler(this, socket);
 			clients.add(client);
 			new Thread(client).start();
 		}
-		serverSocket.close();
 	}
 
-	private synchronized void setRunning() {
+	private synchronized ServerSocket createServerSocket() throws IOException {
 		if (running) {
 			throw new IllegalStateException("Already running");
 		}
+
 		running = true;
+		serverSocket = new ServerSocket(port);
+		return serverSocket;
 	}
 	
 	public synchronized boolean isRunning() {
@@ -47,7 +57,14 @@ public class EchoServer {
 	}
 
 	public synchronized void stopServer() throws IOException {
+		if (!running) {
+			throw new IllegalStateException("Not running");
+		}
+
 		running = false;
+		
+		serverSocket.close();
+		serverSocket = null;
 		
 		for (ClientHandler next : clients) {
 			next.stopClient();
