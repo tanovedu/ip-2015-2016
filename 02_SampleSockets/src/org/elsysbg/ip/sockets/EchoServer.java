daf40f04a553abc3288eb2b3fcb7bbdc143cb0e8
Plamen Tanov <tanovedu@gmail.com>
2015-11-06 18:10:41 +0200

    Added loop for handling more than one clients, stopServer does not work

--- 02_SampleSockets/src/org/elsysbg/ip/sockets/EchoServer.java
+++ 02_SampleSockets/src/org/elsysbg/ip/sockets/EchoServer.java
@@ -6,19 +6,23 @@ import java.net.Socket;
 
 public class EchoServer {
 	private final int port;
+	private boolean running;
 
 	public EchoServer(int port) {
 		this.port = port;
 	}
 
 	public void startServer() throws IOException {
+		running = true;
 		final ServerSocket serverSocket =
 			new ServerSocket(port);
-		final Socket socket = serverSocket.accept();
 		
-		final ClientHandler client = new ClientHandler(socket);
-		client.run();
-		
+		while(running) {
+			final Socket socket = serverSocket.accept();
+			
+			final ClientHandler client = new ClientHandler(socket);
+			client.run();
+		}
 		serverSocket.close();
 	}
 
