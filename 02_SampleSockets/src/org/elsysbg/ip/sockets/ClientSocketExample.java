a0ad9d62336765686cb6040cab21df4c01761e17
Plamen Tanov <tanovedu@gmail.com>
2015-11-06 17:15:10 +0200

    IO from standart input/output is replaced by socket IO

--- 02_SampleSockets/src/org/elsysbg/ip/sockets/ClientSocketExample.java
+++ 02_SampleSockets/src/org/elsysbg/ip/sockets/ClientSocketExample.java
@@ -1,21 +1,28 @@
 package org.elsysbg.ip.sockets;
 
+import java.io.IOException;
 import java.io.PrintStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.Scanner;
 
 public class ClientSocketExample {
-	public static void main(String[] args) {
-		final PrintStream out = System.out;
+	public static void main(String[] args) throws UnknownHostException, IOException {
+		final Socket socket = new Socket("www.example.com", 80);
+		final PrintStream out = 
+			new PrintStream(socket.getOutputStream());
 		out.println("GET / HTTP/1.1");
 		out.println("Host:www.example.com");
 		out.println("Connection:close");
 		out.println("");
 		
-		final Scanner scanner = new Scanner(System.in);
+		final Scanner scanner = new Scanner(socket.getInputStream());
 		while (scanner.hasNextLine()) {
 			final String line = scanner.nextLine();
 			System.out.println(line);
 		}
 		scanner.close();
+		out.close();
+		socket.close();
 	}
 }
