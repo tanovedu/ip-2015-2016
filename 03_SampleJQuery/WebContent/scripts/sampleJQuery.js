b652b92cf4aa11cd701bb8af5a05a11c5d26986b
Plamen Tanov <tanovedu@gmail.com>
2015-12-04 16:45:39 +0200

    Using functions in javascript

--- 03_SampleJQuery/WebContent/scripts/sampleJQuery.js
+++ 03_SampleJQuery/WebContent/scripts/sampleJQuery.js
@@ -1,7 +1,15 @@
 $(document).ready(function() {
 	"use strict"
+	// define function:
+	function removeByClassName() {
+	}
+	
+	// call function:
+	removeByClassName();
 	
 	// add new row to the table
 	// very basic example
 	$("table").append("<tr><td>3</td><td>Georgi</td></tr>");
+	
+	
 });
