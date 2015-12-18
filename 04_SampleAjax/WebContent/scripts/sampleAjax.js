33cec190bca52f977ecfbfcb428ecded9a9babdd
Plamen Tanov <tanovedu@gmail.com>
2015-12-18 17:13:10 +0200

    REST: get by id

--- 04_SampleAjax/WebContent/scripts/sampleAjax.js
+++ 04_SampleAjax/WebContent/scripts/sampleAjax.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
 	"use strict";
-	var ENDPOINT = "http://localhost:3000/tasksaa";
-
+	var ENDPOINT = "http://localhost:3000/tasks";
 	// global error handler:
 	$(document).ajaxError(function() {
 		console.log("error: ", arguments);
@@ -19,6 +18,13 @@ $(document).ready(function() {
 		console.log(response);
 	});
 	// read single task
+	$.ajax(ENDPOINT + "/" + 1, {
+		method: "GET",
+		dataType: "json"
+	}).then(function(response) {
+		console.log(response);
+	});
+
 	// add task
 	// delete task
 	// update task
