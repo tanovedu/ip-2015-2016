43f26a24e51cdb15ac28d6e54cc5067e02cc05df
Plamen Tanov <tanovedu@gmail.com>
2015-12-18 18:12:14 +0200

    Added click handler when task is clicked. Read panel is displayed

--- 04_SampleAjax/WebContent/scripts/todoList.js
+++ 04_SampleAjax/WebContent/scripts/todoList.js
@@ -1,6 +1,13 @@
 $(document).ready(function() {
 	"use strict";
 	var ENDPOINT = "http://localhost:3000/tasks";
+	function showPanel(panelName) {
+		var ALL_PANELS = ["emptyPanel", "readPanel", "updatePanel", "createPanel"];
+		_.forEach(ALL_PANELS, function(nextValue) {
+			$("#"+nextValue).hide();
+		});
+		$("#"+panelName).show();
+	}
 
 	$.ajax(ENDPOINT, {
 		method: "GET",
@@ -11,10 +18,13 @@ $(document).ready(function() {
 			newItem.text(task.title);
 			newItem.addClass("list-group-item");
 			newItem.attr("data-task-id", task.id);
-
 			$("#tasksList").append(newItem);
 		}
 		$("#tasksList").html("");
 		_.forEach(response, addTaskToList);
+	});
+
+	$(document).on("click", "#tasksList [data-task-id]", function() {
+		showPanel("readPanel");
 	});
 });
