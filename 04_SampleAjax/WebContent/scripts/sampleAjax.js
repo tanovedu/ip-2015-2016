344afec4204f05e45ce5c0b83dba2ad7f23de597
Plamen Tanov <tanovedu@gmail.com>
2015-12-18 17:05:48 +0200

    Example for adding URL parameters

--- 04_SampleAjax/WebContent/scripts/sampleAjax.js
+++ 04_SampleAjax/WebContent/scripts/sampleAjax.js
@@ -3,6 +3,10 @@ $(document).ready(function() {
 	var ENDPOINT = "http://localhost:3000/tasks";
 	$.ajax(ENDPOINT, {
 		method: "GET",
+		// to add parameters to URL:
+		data: {
+			title: "hello"
+		},
 		dataType: "json"
 	}).then(function(response) {
 		console.log(response);
