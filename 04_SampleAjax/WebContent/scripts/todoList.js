da60bd52b550658b103decbed48c3cb0e8b7a922
Plamen Tanov <tanovedu@gmail.com>
2016-02-09 08:24:00 +0200

    #1 Implement create task functionality

--- 04_SampleAjax/WebContent/scripts/todoList.js
+++ 04_SampleAjax/WebContent/scripts/todoList.js
@@ -25,13 +25,21 @@ $(document).ready(function() {
 			dataType: "json"
 		});
 	}
+	function createTask(task) {
+		return $.ajax(ENDPOINT, {
+			method: "POST",
+			dataType: "json",
+			data: JSON.stringify(task),
+			contentType: "application/json; charset=utf-8"
+		});
+	}
 	function showTaskView(task) {
 		$("#readPanel .task-title").text(task.title);
 		$("#readPanel .task-description").text(task.description);
 		showPanel("readPanel");
 	}
 	function reloadTasks() {
-		listTasks().then(function(response) {
+		return listTasks().then(function(response) {
 			function addTaskToList(task) {
 				var newItem = $("<li />");
 				newItem.text(task.title);
@@ -50,6 +58,22 @@ $(document).ready(function() {
 		});
 		$(".task-action-cancel").click(function() {
 			showPanel("emptyPanel");
+		});
+		$("#addTaskButton").click(function() {
+			$("#createPanel [name='title']").val("");
+			$("#createPanel [name='description']").val("");
+			showPanel("createPanel");
+		});
+		$("#createPanel .task-action-ok").click(function() {
+			var task = {
+				title: $("#createPanel [name='title']").val(),
+				description: $("#createPanel [name='description']").val()
+			};
+			createTask(task).then(function(response) {
+				reloadTasks().then(function() {
+					showTaskView(response);
+				});
+			});
 		});
 	}
 	attachHandlers();
