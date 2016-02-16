4179550b26445ab750b3f996ba881cea0cd1ac98
Plamen Tanov <tanovedu@gmail.com>
2016-02-16 09:30:50 +0200

    Sample REST endpoint - returning task on GET by ID get from the url

--- 05_SampleBackend/src/main/java/org/elsysbg/ip/todo/rest/TasksRest.java
+++ 05_SampleBackend/src/main/java/org/elsysbg/ip/todo/rest/TasksRest.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -22,5 +23,17 @@ public class TasksRest {
 		task.setId(1);
 		
 		return Collections.singletonList(task);
+	}
+	
+	@GET
+	@Path("/{taskId}")
+	@Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+	public Task getTask(@PathParam("taskId") long taskId) {
+		final Task task = new Task();
+		task.setDescription("description");
+		task.setTitle("title");
+		task.setId(taskId);
+		
+		return task;
 	}
 }
