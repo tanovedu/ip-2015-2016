12f80727ed7006f723ab6db2d8f85a087c936131
Plamen Tanov <tanovedu@gmail.com>
2016-02-16 10:03:39 +0200

    Sample REST endpoint - deleting a task on DELETE

--- 05_SampleBackend/src/main/java/org/elsysbg/ip/todo/rest/TasksRest.java
+++ 05_SampleBackend/src/main/java/org/elsysbg/ip/todo/rest/TasksRest.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -41,5 +42,11 @@ public class TasksRest {
 	@Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
 	public Task createTask(Task task) {
 		return tasksService.createTask(task);
+	}
+	
+	@DELETE
+	@Path("/{taskId}")
+	public void deleteTask(@PathParam("taskId") long taskId) {
+		tasksService.deleteTask(taskId);
 	}
 }
