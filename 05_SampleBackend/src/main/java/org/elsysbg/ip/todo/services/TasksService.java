ae92ddddefa6ce108c7f48f86f0125149f9a75bd
Plamen Tanov <tanovedu@gmail.com>
2016-03-01 08:31:09 +0200

    Added skeleton for working with JPA

--- 05_SampleBackend/src/main/java/org/elsysbg/ip/todo/services/TasksService.java
+++ 05_SampleBackend/src/main/java/org/elsysbg/ip/todo/services/TasksService.java
@@ -3,13 +3,36 @@ package org.elsysbg.ip.todo.services;
 import java.util.List;
 
 import javax.inject.Singleton;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 import org.elsysbg.ip.todo.entities.Task;
 
 @Singleton
 public class TasksService {
+	// TODO should be moved to separate Service
+	private final EntityManagerFactory emf;
+	
+	public TasksService() {
+		// this should be done only once in the application:
+		emf = Persistence.
+			createEntityManagerFactory("todolist-jpa");
+	}
 
 	public Task createTask(Task task) {
+		final EntityManager em = emf.createEntityManager();
+		try {
+			em.getTransaction().begin();
+			// do some stuff
+			em.getTransaction().commit();	
+		} finally {
+			if (em.getTransaction().isActive()) {
+				em.getTransaction().rollback();
+			}
+			em.close();
+		}
+		
 		return null;
 	}
 	public List<Task> getTasks() {
