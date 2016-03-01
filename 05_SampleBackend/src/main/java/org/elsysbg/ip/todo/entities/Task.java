aa8685e9d76f7b26c64656521b99eae8642a889d
Plamen Tanov <tanovedu@gmail.com>
2016-03-01 08:22:01 +0200

    Task is annotated as persisted entity, removed DB logic from TasksService

--- 05_SampleBackend/src/main/java/org/elsysbg/ip/todo/entities/Task.java
+++ 05_SampleBackend/src/main/java/org/elsysbg/ip/todo/entities/Task.java
@@ -1,11 +1,23 @@
 package org.elsysbg.ip.todo.entities;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement
+@Entity
 public class Task {
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Id
 	private long id;
+	
+	@Column(nullable = false)
 	private String title;
+	
+	@Column(nullable = false)
 	private String description;
 
 	public long getId() {
--- 05_SampleBackend/src/main/java/org/elsysbg/ip/todo/services/TasksService.java
+++ 05_SampleBackend/src/main/java/org/elsysbg/ip/todo/services/TasksService.java
@@ -1,8 +1,5 @@
 package org.elsysbg.ip.todo.services;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.LinkedList;
 import java.util.List;
 
 import javax.inject.Singleton;
@@ -11,37 +8,19 @@ import org.elsysbg.ip.todo.entities.Task;
 
 @Singleton
 public class TasksService {
-	private long lastId = 0;
-	private List<Task> tasks = Collections.synchronizedList(
-			new LinkedList<Task>());
-	
-	private synchronized long getAndIncrementNextId() {
-		return ++lastId;
-	}
 
 	public Task createTask(Task task) {
-		task.setId(getAndIncrementNextId());
-		tasks.add(task);
-		return task;
+		return null;
 	}
 	public List<Task> getTasks() {
-		return new ArrayList<Task>(tasks);
+		return null;
 	}
 	public Task getTask(long taskId) {
-		for (Task task : tasks) {
-			if (taskId == task.getId()) {
-				return task;
-			}
-		}
-		throw new IllegalArgumentException("No task with id: " + taskId);
+		return null;
 	}
 	public Task updateTask(Task task) {
-		deleteTask(task.getId());
-		tasks.add(task);
-		return task;
+		return null;
 	}
 	public void deleteTask(long taskId) {
-		final Task oldTask = getTask(taskId);
-		tasks.remove(oldTask);
 	}
 }
